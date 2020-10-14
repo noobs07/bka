9f7f074e7db2c8819f52046a4aeed41e80c31a66
fadhlis <fadhlishobirin@gmail.com>
2020-10-15 00:49:12 +0700

    Event Added

---
+++ application/controllers/api/Event.php
@@ -0,0 +1,115 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Event extends REST_Controller {
+
+	private $table = 'event';
+	private $id_column = 'id_event';
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	function index_get() {
+		$draw = (int) $this->get('draw');
+		$limit = (int) $this->get('length');
+		$offset = (int) $this->get('start');
+		$keyword = $this->get('search');
+		$order = $this->get('order');
+
+		$recordsTotal = $this->db->count_all($this->table);
+		$recordsFiltered = $recordsTotal;
+		if(isset($keyword) && $keyword['value'] != '') {
+			$this->db->or_like('judul', $keyword['value']);
+			$this->db->or_like('konten', $keyword['value']);
+			$this->db->or_like('cover', $keyword['value']);
+			$this->db->or_like('posted_date', $keyword['value']);
+			$recordsFiltered = $this->db->count_all_results($this->table);
+		}
+
+		$columns = array('id_event','judul','konten','cover','posted_date');
+		$this->db->select('id_event,judul,konten,concat("'.base_url('assets/uploads/event/').'",cover) as cover,posted_date');
+		if(isset($keyword) && $keyword['value'] != '') {
+			$this->db->or_like('judul', $keyword['value']);
+			$this->db->or_like('konten', $keyword['value']);
+			$this->db->or_like('cover', $keyword['value']);
+			$this->db->or_like('posted_date', $keyword['value']);
+		}
+		if(isset($offset) && $limit != '-1') {
+			$this->db->limit($limit, $offset);
+		} else {
+			$this->db->limit(10, 0);
+		}
+		if(isset($order)) {
+			$this->db->order_by($columns[$order[0]['column']],$order[0]['dir']);
+		}
+		$data = $this->db->get($this->table)->result();
+		$this->response((object)array(
+			'draw' => $draw,
+			'recordsTotal' => $recordsTotal,
+			'recordsFiltered' => $recordsFiltered,
+			'data' => $data,
+		), 200);
+	}
+
+	function one_get(){
+		$id = $this->get('id');
+		$data = $this->db->where($this->id_column,$id)->get($this->table)->row();
+		$this->response($data, 200);
+	}
+
+	function save_post(){
+		$id = $this->input->post('id');
+		$data['judul'] = $this->input->post('judul');
+		$data['konten'] = $this->input->post('konten');
+
+		$permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+		$new_name = substr(str_shuffle($permitted_chars), 0, 11);
+		$this->upload_file_config('./assets/uploads/event/', $new_name);
+
+		if ($this->upload->do_upload('file')) {
+			$uploaded = $this->upload->data();
+			$data['cover'] = $uploaded['file_name'];
+			$old = $this->get_one($id);
+			if ($old) {
+				unlink('./assets/uploads/event/'.$old->foto);
+			}
+		}
+
+		$result = false;
+		if ($id) {
+			$data[$this->id_column] = $id;
+			$this->db->where($this->id_column, $id);
+			$result = $this->db->update($this->table, $data);
+		} else {
+			$data['posted_date'] = date("Y-m-d h:i:s");
+			$result = $this->db->insert($this->table, $data);
+		}
+
+		if ($result) {
+			$this->response($files, 200);
+		} else {
+			$this->response($data, 400);
+		}
+	}
+
+	function delete_post(){
+		$id = $this->input->post('id');
+		$this->db->where($this->id_column, $id);
+		$result = $this->db->delete($this->table);
+		if ($result) {
+			$this->response(true, 200);
+		} else {
+			$this->response(null, 400);
+		}
+	}
+
+	function get_one($id){
+		return $this->db->where($this->id_column,$id)->get($this->table)->row();
+	}
+
+}
---
+++ application/views/admin/event/v_event.php
@@ -0,0 +1,32 @@
+<div class="content-wrapper">
+	<div class="content-header"></div>
+
+	<section class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-12">
+					<div class="card">
+						<div class="card-header">
+							<h2 class="card-title">Daftar Event</h2>
+							<div class="float-right">
+								<button class="btn btn-warning btn-sm" onclick="refreshTable()">Refresh</button>
+								&nbsp;
+								<button class="btn btn-success btn-sm" data-toggle="modal" data-target="#form_modal">Tambah</button>
+							</div>
+						</div>
+						<!-- /.card-header -->
+						<div class="card-body">
+							<table id="table" class="table table-bordered table-hover" style="width:100%"></table>
+						</div>
+						<!-- /.card-body -->
+					</div>
+					<!-- /.card -->
+
+				</div>
+				<!-- /.col -->
+			</div>
+			<!-- /.row -->
+		</div>
+		<!-- /.container-fluid -->
+	</section>
+</div>a
