a74c59330c7bc91fce04c88b9fc5e9ef571fd09f
fadhlis <fadhlishobirin@gmail.com>
2020-11-03 15:08:27 +0700

    update bka

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -19,7 +19,39 @@ class Dashboard extends MY_Controller {
 		$data['jumlah_bigroot'] = $this->db->where('jenis','1')->count_all_results('produk');
 		$data['jumlah_vermont'] = $this->db->where('jenis','2')->count_all_results('produk');
 
+		$pengaturan = $this->db->limit(1)->get('pengaturan')->row();
+		if ($pengaturan) {
+			$data['pengaturan'] = $pengaturan;
+		} else {
+			$data['pengaturan'] = false;
+		}
 
 		$this->loadview($data, $page, $js);
+	}
+
+	public function update_link_wa()
+	{
+		$id = $this->input->post('id');
+		$data['link_toko'] = $this->input->post('link_toko');
+		$data['whatsapp'] = $this->input->post('whatsapp');
+
+		$this->db->where('id',$id);
+		$result  = $this->db->update('pengaturan', $data);
+
+		if ($result) {
+			echo json_encode(
+				array(
+					'status' => true,
+					'msg' => 'Berhasil update',
+				)
+			);
+		} else {
+			echo json_encode(
+				array(
+					'status' => false,
+					'msg' => 'Gagal update',
+				)
+			);
+		}
 	}
 }
--- application/views/admin/dashboard/js_dashboard.php
+++ application/views/admin/dashboard/js_dashboard.php
@@ -1,6 +1,63 @@
 <script type="text/javascript">
 	$(document).ready( function () {
 
+		$('#cancel_btn').click(e=>{
+			$('#cancel_btn').hide()
+			$('#save_btn').hide()
+			$('input[name="id"]').prop('readonly', true)
+			$('input[name="link_toko"]').prop('readonly', true)
+			$('input[name="whatsapp"]').prop('readonly', true)
+
+			$('#edit_btn').show()
+		})
+
+		$('#edit_btn').click(e=>{
+			$('#cancel_btn').show()
+			$('#save_btn').show()
+			$('input[name="id"]').prop('readonly', false)
+			$('input[name="link_toko"]').prop('readonly', false)
+			$('input[name="whatsapp"]').prop('readonly', false)
+
+			$('#edit_btn').hide()
+		})
+
+		$('#save_btn').click(e=>{
+			const save_btn = $(e.target)
+			$.ajax({
+				url: '<?= base_url('admin/'.$module.'/update_link_wa') ?>',
+				data: {
+					id: $('input[name="id"]').val(),
+					link_toko: $('input[name="link_toko"]').val(),
+					whatsapp: $('input[name="whatsapp"]').val(),
+				},
+				type: 'POST',
+				beforeSend: function (xhr, settings){
+					save_btn.prop('disabled', true)
+					save_btn.text('Menyimpan')
+				},
+				success: function(response){
+					$('#cancel_btn').hide()
+					$('#save_btn').hide()
+					$('input[name="id"]').prop('readonly', true)
+					$('input[name="link_toko"]').prop('readonly', true)
+					$('input[name="whatsapp"]').prop('readonly', true)
+
+					$('#edit_btn').show()
+
+					const parsed = JSON.parse(response)
+					if (parsed['status']) {
+						toastr.success(parsed['msg'])
+					}else {
+						toastr.error(parsed['msg'])
+					}
+				},
+				error: function(error){
+					save_btn.prop('disabled', false)
+					save_btn.text('Simpan')
+					toastr.error('Gagal disimpan')
+				}
+			})
+		})
 
 	});
 </script>
